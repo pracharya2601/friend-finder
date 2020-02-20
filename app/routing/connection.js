d492b937e76d2d0d09eab330dd73d7f51e074b04
Prakash Acharya <pracharya2601@gmail.com>
2020-02-19 16:51:20 -0800

    fix error

--- app/routing/connection.js
+++ app/routing/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password", //put password of MySql
+  password: "password-here", //put password of MySql
   database: "Friends"
 });
 }
